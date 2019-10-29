09c962079d9b5aa0da727476416c1088192ae836
LuisaMicolta <luisafer.micolta@gmail.com>
2019-10-29 10:19:00 -0500

    Segundo Test

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -0,0 +1,41 @@
+package com.hbt.semillero.servicios;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	public void shouldAnswerWithTrue() {
+
+	}
+
+	@Test
+	public void primeraPU() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L;
+		Long sumando2 = 50L;
+		Long resultado = sumando1 + sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+		resultadoEsperado=200L;
+		Assert.assertNotEquals(resultado, resultadoEsperado);
+
+	}
+
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+		return cadenaInvertida;
+	}
+	
+	@Test
+	public void segundaPU() {
+		String cadena = "Luisa Micolta";		
+		String cadenaInvertida = invertirCadena(cadena);		
+       
+		Assert.assertEquals(cadenaInvertida, "atlociM asiuL");
+		
+	}
+
+}
