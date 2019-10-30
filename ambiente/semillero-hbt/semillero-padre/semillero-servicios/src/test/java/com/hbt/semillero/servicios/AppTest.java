d5a19694a91edce21d30f15c04094e1b68699e2b
LuisaMicolta <luisafer.micolta@gmail.com>
2019-10-30 12:41:42 -0500

    Ejercicio Sesion 2 Java

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -49,20 +49,27 @@ public class AppTest {
 		EstadoEnum estadoEnumActivo = EstadoEnum.ACTIVO;
 		EstadoEnum estadoEnumInactivo = EstadoEnum.INACTIVO;
 
-		// Assert que valida que el valor de estadoEnumActivo es ACTIVO
 
+		/**
+		 * Assert que valida que el valor de estadoEnumActivo es ACTIVO
+		 */
 		Assert.assertNotSame(estadoEnumActivo, "ACTIVO", "El estado es ACTIVO");
 		System.out.println("\nNombre de la constante = " + estadoEnumActivo);
 
-		// Assert valida la posicion de la variable estadoEnumInactivo dentro
-		// del Array la cual es 1.
 
+		/**
+		 *  Assert valida la posicion de las variables Enum
+		 */
 		Assert.assertEquals(estadoEnumInactivo.ordinal(), 1);
 		System.out.println("\nLa posicion del Enum ACTIVO ES = " + estadoEnumActivo.ordinal());
 		System.out.println("La posicion del Enum INACTIVO ES = " + estadoEnumInactivo.ordinal() + "\n");
 
-		// Recorre el arreglo estadoEnum y muestra sus valores
 
+
+		/**
+		 * Recorre el arreglo estadoEnum y muestra sus valores
+		 */
+		
 		for (EstadoEnum estadoEnum : EstadoEnum.values()) {
 
 			System.out.println("Valor arreglo ENUM =" + estadoEnum);
